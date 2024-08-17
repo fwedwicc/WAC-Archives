8e3f7c9383908978a967ea870623d65554f934b5
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-08-18 03:02:45 +0800

    Changed the color palette for overall dark mode styles

--- src/dashboard.php
+++ src/dashboard.php
@@ -1,36 +1,36 @@
 <!-- Sample Content -->
 <div class="space-y-8">
   <div class="space-y-1.5">
-    <h2 class="text-3xl dark:text-neutral-200 text-neutral-800 leading-none font-semibold">Dashboard</h2>
-    <p class="dark:text-neutral-400 text-neutral-600">This page contains dashboard content.</p>
+    <h2 class="text-3xl dark:text-gray-200 text-gray-800 leading-none font-semibold">Dashboard</h2>
+    <p class="dark:text-gray-400 text-gray-600">This page contains dashboard content.</p>
   </div>
   <!-- Sample Containers -->
-  <div class="p-4 border-2 dark:border-neutral-600 border-neutral-300 border-dashed rounded-lg">
+  <div class="p-4 border-2 dark:border-gray-600 border-gray-300 border-dashed rounded-lg">
     <div class="grid grid-cols-3 gap-4 mb-4">
-      <div class="flex items-center justify-center h-24 rounded dark:bg-neutral-800 bg-neutral-200">
-        <p class="text-2xl text-neutral-500">
+      <div class="flex items-center justify-center h-24 rounded dark:bg-gray-900/40 bg-gray-200">
+        <p class="text-2xl text-gray-500">
           <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
           </svg>
         </p>
       </div>
-      <div class="flex items-center justify-center h-24 rounded dark:bg-neutral-800 bg-neutral-200">
-        <p class="text-2xl text-neutral-500">
+      <div class="flex items-center justify-center h-24 rounded dark:bg-gray-900/40 bg-gray-200">
+        <p class="text-2xl text-gray-500">
           <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
           </svg>
         </p>
       </div>
-      <div class="flex items-center justify-center h-24 rounded dark:bg-neutral-800 bg-neutral-200">
-        <p class="text-2xl text-neutral-500">
+      <div class="flex items-center justify-center h-24 rounded dark:bg-gray-900/40 bg-gray-200">
+        <p class="text-2xl text-gray-500">
           <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
           </svg>
         </p>
       </div>
     </div>
-    <div class="flex items-center justify-center h-48 rounded dark:bg-neutral-800 bg-neutral-200">
-      <p class="text-2xl text-neutral-500">
+    <div class="flex items-center justify-center h-48 rounded dark:bg-gray-900/40 bg-gray-200">
+      <p class="text-2xl text-gray-500">
         <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
         </svg>
--- src/layout.php
+++ src/layout.php
@@ -8,28 +8,36 @@
   <link href="./output.css" rel="stylesheet">
 </head>
 
-<body class="dark:bg-neutral-800 bg-neutral-50">
+<body class="dark:bg-[#192231] bg-neutral-50">
   <!-- Sidebar -->
   <aside class="lg:block hidden w-64 fixed h-screen p-5 space-y-7">
     <!-- Logo -->
-    <img src="../assets/FM-logo.png" alt="FM Logo" class="w-7 h-auto mt-4 ml-4">
+    <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-7 h-auto mt-4 ml-4">
+      <path d="M11.5762 7.01086C11.5762 3.13887 14.715 0 18.587 0V0V30H11.5762V7.01086Z" fill="#6366f1" />
+      <rect y="15.8986" width="7.01087" height="14.1014" fill="#6366f1" />
+      <rect x="22.9893" y="15.8986" width="7.01087" height="14.1014" fill="#6366f1" />
+      <path
+        d="M24.0553 9.95392C27.3385 9.95392 30 12.6155 30 15.8986V15.8986L1.52588e-05 15.8986V15.8986C1.52588e-05 12.6155 2.66155 9.95392 5.94472 9.95392L24.0553 9.95392Z"
+        fill="#6366f1" />
+      <rect x="22.9893" width="5.9447" height="5.38043" transform="rotate(90 22.9893 0)" fill="#6366f1" />
+    </svg>
     <!-- Navigation Buttons -->
     <div class="space-y-2">
-      <span class="text-neutral-400 text-xs">Active tabs</span>
+      <span class="text-gray-400 text-xs font-medium">Active tabs</span>
       <nav class="flex flex-col gap-1" aria-label="Tabs" role="tablist" aria-orientation="vertical">
-        <button type="button" class="hs-tab-active:bg-green-500/10 hs-tab-active:border-l-green-500 hs-tab-active:border-l-2 hs-tab-active:text-green-400 py-2.5 px-4 inline-flex items-center gap-2.5 hover:bg-neutral-700/20 text-sm font-medium text-center text-neutral-400 rounded-md hover:text-neutral-200 focus:outline-none focus:text-gray-700 disabled:opacity-50 disabled:pointer-events-none active transition ease-in-out duration-300" id="dashboard-item" aria-selected="true" data-hs-tab="#dashboard-tab" aria-controls="dashboard-tab" role="tab">
+        <button type="button" class="hs-tab-active:bg-indigo-500/10 hs-tab-active:border-l-indigo-500 hs-tab-active:border-l-2 hs-tab-active:text-indigo-400 py-2.5 px-4 inline-flex items-center gap-2.5 hover:bg-gray-700/10 text-sm font-medium text-center text-gray-400 rounded-md hover:text-gray-200 focus:outline-none focus:text-gray-700 disabled:opacity-50 disabled:pointer-events-none active transition ease-in-out duration-300" id="dashboard-item" aria-selected="true" data-hs-tab="#dashboard-tab" aria-controls="dashboard-tab" role="tab">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
             <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 0 0 2.25-2.25V6a2.25 2.25 0 0 0-2.25-2.25H6A2.25 2.25 0 0 0 3.75 6v2.25A2.25 2.25 0 0 0 6 10.5Zm0 9.75h2.25A2.25 2.25 0 0 0 10.5 18v-2.25a2.25 2.25 0 0 0-2.25-2.25H6a2.25 2.25 0 0 0-2.25 2.25V18A2.25 2.25 0 0 0 6 20.25Zm9.75-9.75H18a2.25 2.25 0 0 0 2.25-2.25V6A2.25 2.25 0 0 0 18 3.75h-2.25A2.25 2.25 0 0 0 13.5 6v2.25a2.25 2.25 0 0 0 2.25 2.25Z" />
           </svg>
           Dashboard
         </button>
-        <button type="button" class="hs-tab-active:bg-green-500/10 hs-tab-active:border-l-green-500 hs-tab-active:border-l-2 hs-tab-active:text-green-400 py-2.5 px-4 inline-flex items-center gap-2.5 hover:bg-neutral-700/20 text-sm font-medium text-center text-neutral-400 rounded-md hover:text-neutral-200 focus:outline-none focus:text-gray-700 disabled:opacity-50 disabled:pointer-events-none transition ease-in-out duration-300" id="card-type-tab-item-2" aria-selected="false" data-hs-tab="#card-type-tab-2" aria-controls="card-type-tab-2" role="tab">
+        <button type="button" class="hs-tab-active:bg-indigo-500/10 hs-tab-active:border-l-indigo-500 hs-tab-active:border-l-2 hs-tab-active:text-indigo-400 py-2.5 px-4 inline-flex items-center gap-2.5 hover:bg-gray-700/10 text-sm font-medium text-center text-gray-400 rounded-md hover:text-gray-200 focus:outline-none focus:text-gray-700 disabled:opacity-50 disabled:pointer-events-none transition ease-in-out duration-300" id="card-type-tab-item-2" aria-selected="false" data-hs-tab="#card-type-tab-2" aria-controls="card-type-tab-2" role="tab">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
             <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
           </svg>
           Di ko na alam
         </button>
-        <button type="button" class="hs-tab-active:bg-green-500/10 hs-tab-active:border-l-green-500 hs-tab-active:border-l-2 hs-tab-active:text-green-400 py-2.5 px-4 inline-flex items-center gap-2.5 hover:bg-neutral-700/20 text-sm font-medium text-center text-neutral-400 rounded-md hover:text-neutral-200 focus:outline-none focus:text-gray-700 disabled:opacity-50 disabled:pointer-events-none transition ease-in-out duration-300" id="card-type-tab-item-3" aria-selected="false" data-hs-tab="#card-type-tab-3" aria-controls="card-type-tab-3" role="tab">
+        <button type="button" class="hs-tab-active:bg-indigo-500/10 hs-tab-active:border-l-indigo-500 hs-tab-active:border-l-2 hs-tab-active:text-indigo-400 py-2.5 px-4 inline-flex items-center gap-2.5 hover:bg-gray-700/10 text-sm font-medium text-center text-gray-400 rounded-md hover:text-gray-200 focus:outline-none focus:text-gray-700 disabled:opacity-50 disabled:pointer-events-none transition ease-in-out duration-300" id="card-type-tab-item-3" aria-selected="false" data-hs-tab="#card-type-tab-3" aria-controls="card-type-tab-3" role="tab">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
             <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
           </svg>
@@ -43,14 +51,14 @@
     <!-- Content Header -->
     <header class="flex justify-end items-start gap-3 h-12">
       <!-- Theme Toggle -->
-      <button type="button" class="hs-dark-mode-active:hidden block hs-dark-mode font-medium text-neutral-300 rounded-full hover:bg-neutral-500/20 transition duration-300 ease-in-out focus:outline-none focus:bg-gray-200" data-hs-theme-click-value="dark">
+      <button type="button" class="hs-dark-mode-active:hidden block hs-dark-mode font-medium dark:text-gray-300 text-gray-700 rounded-full hover:bg-gray-500/20 transition duration-300 ease-in-out focus:outline-none focus:bg-gray-200" data-hs-theme-click-value="dark">
         <span class="group inline-flex shrink-0 justify-center items-center size-8">
           <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
             <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"></path>
           </svg>
         </span>
       </button>
-      <button type="button" class="hs-dark-mode-active:block hidden hs-dark-mode font-medium text-neutral-300 rounded-full hover:bg-neutral-500/20 transition duration-300 ease-in-out focus:outline-none focus:bg-gray-200" data-hs-theme-click-value="light">
+      <button type="button" class="hs-dark-mode-active:block hidden hs-dark-mode font-medium dark:text-gray-300 text-gray-700 rounded-full hover:bg-gray-500/20 transition duration-300 ease-in-out focus:outline-none focus:bg-gray-200" data-hs-theme-click-value="light">
         <span class="group inline-flex shrink-0 justify-center items-center size-8">
           <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
             <circle cx="12" cy="12" r="4"></circle>
@@ -66,11 +74,11 @@
         </span>
       </button>
       <!-- Profile Dropdown -->
-      <span class="inline-flex items-center justify-center size-8 text-xs font-semibold rounded-full leading-none border border-neutral-300 bg-green-600 text-white">
+      <span class="inline-flex items-center justify-center size-8 text-xs font-semibold rounded-full leading-none border border-gray-300 bg-indigo-600 text-white">
         FM
       </span>
     </header>
-    <div class="dark:bg-neutral-700/20 bg-neutral-100 border dark:border-neutral-600/20 border-none p-8 rounded-xl h-full custom-scrollbar overflow-auto">
+    <div class="dark:bg-gray-800 bg-gray-100 border dark:border-gray-700/50 p-8 rounded-xl h-full custom-scrollbar overflow-auto">
       <!-- Tab 1 -->
       <div id="dashboard-tab" role="tabpanel" aria-labelledby="dashboard-item">
         <?php
