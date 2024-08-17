c31236033739f5c6ffc538a354408cce19021e72
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-08-18 00:36:48 +0800

    Installed additional packages for theme switch, also tested dark and light mode styles

--- src/dashboard.php
+++ src/dashboard.php
@@ -1,27 +1,27 @@
 <!-- Sample Content -->
 <div class="space-y-8">
   <div class="space-y-1.5">
-    <h2 class="text-3xl text-neutral-100 font-semibold">Dashboard</h2>
-    <p class="text-neutral-300">This page contains dashboard content.</p>
+    <h2 class="text-3xl dark:text-neutral-200 text-neutral-800 leading-none font-semibold">Dashboard</h2>
+    <p class="dark:text-neutral-400 text-neutral-600">This page contains dashboard content.</p>
   </div>
   <!-- Sample Containers -->
-  <div class="p-4 border-2 border-neutral-600 border-dashed rounded-lg">
+  <div class="p-4 border-2 dark:border-neutral-600 border-neutral-300 border-dashed rounded-lg">
     <div class="grid grid-cols-3 gap-4 mb-4">
-      <div class="flex items-center justify-center h-24 rounded bg-neutral-800">
+      <div class="flex items-center justify-center h-24 rounded dark:bg-neutral-800 bg-neutral-200">
         <p class="text-2xl text-neutral-500">
           <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
           </svg>
         </p>
       </div>
-      <div class="flex items-center justify-center h-24 rounded bg-neutral-800">
+      <div class="flex items-center justify-center h-24 rounded dark:bg-neutral-800 bg-neutral-200">
         <p class="text-2xl text-neutral-500">
           <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
           </svg>
         </p>
       </div>
-      <div class="flex items-center justify-center h-24 rounded bg-neutral-800">
+      <div class="flex items-center justify-center h-24 rounded dark:bg-neutral-800 bg-neutral-200">
         <p class="text-2xl text-neutral-500">
           <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
@@ -29,7 +29,7 @@
         </p>
       </div>
     </div>
-    <div class="flex items-center justify-center h-48 rounded bg-neutral-800">
+    <div class="flex items-center justify-center h-48 rounded dark:bg-neutral-800 bg-neutral-200">
       <p class="text-2xl text-neutral-500">
         <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     "./src/**/*.{html,php,js}",
     'node_modules/preline/dist/*.js',
