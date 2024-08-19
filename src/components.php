4cb9ecb61d4b72c5f602d2b1e5d78e0a32a79849
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-08-20 00:25:55 +0800

    Refactored tailwind classes in some pages especially in forms' content

--- src/components.php
+++ src/components.php
@@ -1,8 +1,8 @@
 <!-- Sample Content -->
 <div class="space-y-8">
   <div class="space-y-1.5">
-    <h2 class="text-3xl dark:text-gray-200 text-gray-800 leading-none font-semibold">Components</h2>
-    <p class="dark:text-gray-400 text-gray-600">This page contains different components for web app.</p>
+    <h2>Components</h2>
+    <p>This page contains different components for web app.</p>
   </div>
   <!-- Sample Containers -->
 
--- src/dashboard.php
+++ src/dashboard.php
@@ -1,8 +1,8 @@
 <!-- Sample Content -->
 <div class="space-y-8">
   <div class="space-y-1.5">
-    <h2 class="text-3xl dark:text-gray-200 text-gray-800 leading-none font-semibold">Dashboard</h2>
-    <p class="dark:text-gray-400 text-gray-600">This page contains dashboard content.</p>
+    <h2>Dashboard</h2>
+    <p>This page contains dashboard content.</p>
   </div>
   <!-- Sample Containers -->
   <div class="p-4 border-2 dark:border-gray-600 border-gray-300 border-dashed rounded-lg">
