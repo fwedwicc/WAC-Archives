01068cb748d0fe5cf107db60c5cb89faf7944a1b
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-08-19 16:01:04 +0800

    Installed overlay package for sidebar, also added some assets and modified existing styles for better UI

--- src/layout.php
+++ src/layout.php
@@ -5,86 +5,46 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>FM | WAC Archives</title>
+  <link rel="shortcut icon" href="../public/favicon.ico" type="image/x-icon">
   <link href="./output.css" rel="stylesheet">
   <script src="./js/nav.js"></script>
 </head>
 
 <body class="dark:bg-[#192231] bg-gray-50">
-  <!-- Sidebar -->
+  <!-- Desktop Sidebar -->
   <aside class="lg:block hidden w-64 fixed h-screen p-5 space-y-7">
-    <!-- Logo -->
-    <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-7 h-auto mt-4 ml-4">
-      <path d="M11.5762 7.01086C11.5762 3.13887 14.715 0 18.587 0V0V30H11.5762V7.01086Z" fill="#6366f1" />
-      <rect y="15.8986" width="7.01087" height="14.1014" fill="#6366f1" />
-      <rect x="22.9893" y="15.8986" width="7.01087" height="14.1014" fill="#6366f1" />
-      <path
-        d="M24.0553 9.95392C27.3385 9.95392 30 12.6155 30 15.8986V15.8986L1.52588e-05 15.8986V15.8986C1.52588e-05 12.6155 2.66155 9.95392 5.94472 9.95392L24.0553 9.95392Z"
-        fill="#6366f1" />
-      <rect x="22.9893" width="5.9447" height="5.38043" transform="rotate(90 22.9893 0)" fill="#6366f1" />
-    </svg>
-    <!-- Navigation Buttons -->
-    <div class="space-y-2">
-      <span class="dark:text-gray-400 text-gray-600 text-xs">Active tabs</span>
-      <nav class="flex flex-col gap-1" aria-label="Tabs" role="tablist" aria-orientation="vertical">
-        <button type="button" class="hs-tab-active:bg-indigo-500/10 dark:hs-tab-active:text-indigo-400 hs-tab-active:text-indigo-500 py-2.5 px-4 inline-flex items-center gap-2.5 text-sm font-medium text-center dark:text-gray-400 text-gray-600 rounded-md dark:hover:text-gray-200 hover:text-indigo-500 focus:outline-none focus:text-gray-700 disabled:opacity-50 disabled:pointer-events-none transition ease-in-out duration-300" id="dashboard-item" data-tab-id="dashboard-tab" data-hs-tab="#dashboard-tab" aria-controls="dashboard-tab" role="tab">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 0 0 2.25-2.25V6a2.25 2.25 0 0 0-2.25-2.25H6A2.25 2.25 0 0 0 3.75 6v2.25A2.25 2.25 0 0 0 6 10.5Zm0 9.75h2.25A2.25 2.25 0 0 0 10.5 18v-2.25a2.25 2.25 0 0 0-2.25-2.25H6a2.25 2.25 0 0 0-2.25 2.25V18A2.25 2.25 0 0 0 6 20.25Zm9.75-9.75H18a2.25 2.25 0 0 0 2.25-2.25V6A2.25 2.25 0 0 0 18 3.75h-2.25A2.25 2.25 0 0 0 13.5 6v2.25a2.25 2.25 0 0 0 2.25 2.25Z" />
-          </svg>
-          Dashboard
-        </button>
-        <button type="button" class="hs-tab-active:bg-indigo-500/10 dark:hs-tab-active:text-indigo-400 hs-tab-active:text-indigo-500 py-2.5 px-4 inline-flex items-center gap-2.5 text-sm font-medium text-center dark:text-gray-400 text-gray-600 rounded-md dark:hover:text-gray-200 hover:text-indigo-500 focus:outline-none focus:text-gray-700 disabled:opacity-50 disabled:pointer-events-none transition ease-in-out duration-300" id="components-item" data-tab-id="components-tab" data-hs-tab="#components-tab" aria-controls="components-tab" role="tab">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 7.125C2.25 6.504 2.754 6 3.375 6h6c.621 0 1.125.504 1.125 1.125v3.75c0 .621-.504 1.125-1.125 1.125h-6a1.125 1.125 0 0 1-1.125-1.125v-3.75ZM14.25 8.625c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v8.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 0 1-1.125-1.125v-8.25ZM3.75 16.125c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 0 1-1.125-1.125v-2.25Z" />
-          </svg>
-          Components
-        </button>
-        <button type="button" class="hs-tab-active:bg-indigo-500/10 dark:hs-tab-active:text-indigo-400 hs-tab-active:text-indigo-500 py-2.5 px-4 inline-flex items-center gap-2.5 text-sm font-medium text-center dark:text-gray-400 text-gray-600 rounded-md dark:hover:text-gray-200 hover:text-indigo-500 focus:outline-none focus:text-gray-700 disabled:opacity-50 disabled:pointer-events-none transition ease-in-out duration-300" id="forms-item" data-tab-id="forms-tab" data-hs-tab="#forms-tab" aria-controls="forms-tab" role="tab">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 3.75V16.5L12 14.25 7.5 16.5V3.75m9 0H18A2.25 2.25 0 0 1 20.25 6v12A2.25 2.25 0 0 1 18 20.25H6A2.25 2.25 0 0 1 3.75 18V6A2.25 2.25 0 0 1 6 3.75h1.5m9 0h-9" />
-          </svg>
-          Forms
-        </button>
-      </nav>
-    </div>
+    <?php include './sidebar.php'; ?>
   </aside>
-  <!-- Main Content -->
+  <!-- // Desktop Sidebar -->
+  <!-- Mobile Sidebar -->
+  <div id="hs-offcanvas-custom-backdrop-color" class="hs-overlay hs-overlay-open:translate-x-0 hs-overlay-backdrop-open:bg-gray-900/80 hidden -translate-x-full fixed top-0 start-0 transition-all duration-300 transform h-full max-w-64 w-full p-5 space-y-7 z-[80] bg-gray-50 border-e dark:bg-gray-800 dark:border-gray-700 dark:hs-overlay-backdrop-open:bg-gray-900/80" role="dialog" tabindex="-1" aria-labelledby="hs-offcanvas-custom-backdrop-color-label">
+    <?php include './sidebar.php'; ?>
+  </div>
+  <!-- // Mobile Sidebar -->
+
+  <!-- Main Layout -->
   <div class="lg:py-4 b-4 lg:pr-4 lg:p-0 p-4 lg:ml-64 h-screen">
     <div class="relative dark:bg-gray-800 bg-white border dark:border-gray-700/50 border-gray-200 rounded-xl h-full custom-scrollbar overflow-auto">
       <!-- Content Header -->
-      <header class="dark:bg-gray-800 bg-white sticky top-0 py-4 px-8 flex justify-between items-center dark:border-gray-700/60 border-gray-200 border-b w-full">
-        <!-- Breadcrumbs -->
-        <ol class="flex items-center whitespace-nowrap">
-          <li class="inline-flex items-center">
-            <a class="flex items-center text-sm text-gray-500 hover:text-indigo-600 focus:outline-none focus:text-indigo-600 dark:text-gray-500 dark:hover:text-indigo-500 dark:focus:text-indigo-500" href="#">
-              Dashboard
-            </a>
-            <svg class="shrink-0 mx-2 size-4 text-gray-400 dark:text-gray-600" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-              <path d="m9 18 6-6-6-6"></path>
+      <header class="dark:bg-gray-800/70 backdrop-blur-md bg-white/70 sticky top-0 py-4 px-8 flex justify-between items-center dark:border-gray-700/60 border-gray-200 border-b w-full">
+        <div>
+          <!-- Sidebar Toggle Button for Mobile -->
+          <button type="button" class="lg:hidden flex shrink-0 justify-center items-center gap-2 size-8 text-sm font-medium rounded-lg border border-transparent dark:text-gray-400 text-gray-600 hover:bg-gray-400/20 dark:hover:bg-gray-400/20 focus:ring-2 focus:ring-gray-400/20 focus:outline-none disabled:opacity-50 disabled:pointer-events-none transition duration-300 ease-in-out" aria-haspopup="dialog" aria-expanded="false" aria-controls="hs-offcanvas-custom-backdrop-color" data-hs-overlay="#hs-offcanvas-custom-backdrop-color">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="shrink-0 size-5">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12" />
             </svg>
-          </li>
-          <li class="inline-flex items-center">
-            <a class="flex items-center text-sm text-gray-500 hover:text-indigo-600 focus:outline-none focus:text-indigo-600 dark:text-gray-500 dark:hover:text-indigo-500 dark:focus:text-indigo-500" href="#">
-              App Center
-              <svg class="shrink-0 mx-2 size-4 text-gray-400 dark:text-gray-600" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                <path d="m9 18 6-6-6-6"></path>
-              </svg>
-            </a>
-          </li>
-          <li class="inline-flex items-center text-sm font-semibold text-gray-800 truncate dark:text-gray-200" aria-current="page">
-            Application
-          </li>
-        </ol>
-        <!-- End of Breadcrumbs -->
+          </button>
+        </div>
         <div class="flex items-center gap-3">
           <!-- Theme Toggle -->
-          <button type="button" class="hs-dark-mode-active:hidden block hs-dark-mode font-medium dark:text-gray-400 text-gray-600 rounded-full hover:bg-gray-400/20 transition duration-300 ease-in-out focus:outline-none focus:bg-gray-200" data-hs-theme-click-value="dark">
+          <button type="button" class="hs-dark-mode-active:hidden block hs-dark-mode font-medium dark:text-gray-400 text-gray-600 rounded-full hover:bg-gray-400/20 transition duration-300 ease-in-out focus:ring-2 focus:ring-gray-400/20 focus:outline-none" data-hs-theme-click-value="dark">
             <span class="group inline-flex shrink-0 justify-center items-center size-8">
               <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                 <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"></path>
               </svg>
             </span>
           </button>
-          <button type="button" class="hs-dark-mode-active:block hidden hs-dark-mode font-medium dark:text-gray-400 text-gray-600 rounded-full hover:bg-gray-400/20 transition duration-300 ease-in-out focus:outline-none focus:bg-gray-200" data-hs-theme-click-value="light">
+          <button type="button" class="hs-dark-mode-active:block hidden hs-dark-mode font-medium dark:text-gray-400 text-gray-600 rounded-full hover:bg-gray-400/20 transition duration-300 ease-in-out focus:ring-2 focus:ring-gray-400/20 focus:outline-none" data-hs-theme-click-value="light">
             <span class="group inline-flex shrink-0 justify-center items-center size-8">
               <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                 <circle cx="12" cy="12" r="4"></circle>
@@ -144,7 +104,7 @@
           <!-- End of Profile Dropdown -->
         </div>
       </header>
-      <!-- End of Content Header -->
+      <!-- // Content Header -->
       <!-- Tab Content -->
       <div class="p-8">
         <!-- Tab 1 -->
@@ -160,11 +120,13 @@
           <?php include_once './forms.php'; ?>
         </div>
       </div>
-      <!-- End of Tab Content -->
+      <!-- // Tab Content -->
     </div>
   </div>
-  <!-- End of Main Content -->
+  <!-- // Main Layout -->
   <script src="../node_modules/preline/dist/preline.js"></script>
+  <script src="../scripts/js/open-modals-on-init.js"></script>
+
 </body>
 
 </html>
---
+++ src/sidebar.php
@@ -0,0 +1,27 @@
+<!-- Logo -->
+<img src="./assets/FM-logo.svg" alt="FM Logo" class="w-7 h-auto mt-4 ml-4">
+<!-- Navigation Buttons -->
+<div class="space-y-2">
+  <span class="dark:text-gray-400 text-gray-600 text-xs">Active tabs</span>
+  <nav class="flex flex-col gap-1" aria-label="Tabs" role="tablist" aria-orientation="vertical">
+    <button type="button" class="hs-tab-active:bg-indigo-500/10 dark:hs-tab-active:text-indigo-400 hs-tab-active:text-indigo-500 py-2.5 px-4 inline-flex items-center gap-2.5 text-sm font-medium text-center dark:text-gray-400 text-gray-600 rounded-md dark:hover:text-gray-200 hover:text-indigo-500 focus:outline-none focus:text-gray-700 disabled:opacity-50 disabled:pointer-events-none transition ease-in-out duration-300" id="dashboard-item" data-tab-id="dashboard-tab" data-hs-tab="#dashboard-tab" aria-controls="dashboard-tab" role="tab">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 0 0 2.25-2.25V6a2.25 2.25 0 0 0-2.25-2.25H6A2.25 2.25 0 0 0 3.75 6v2.25A2.25 2.25 0 0 0 6 10.5Zm0 9.75h2.25A2.25 2.25 0 0 0 10.5 18v-2.25a2.25 2.25 0 0 0-2.25-2.25H6a2.25 2.25 0 0 0-2.25 2.25V18A2.25 2.25 0 0 0 6 20.25Zm9.75-9.75H18a2.25 2.25 0 0 0 2.25-2.25V6A2.25 2.25 0 0 0 18 3.75h-2.25A2.25 2.25 0 0 0 13.5 6v2.25a2.25 2.25 0 0 0 2.25 2.25Z" />
+      </svg>
+      Dashboard
+    </button>
+    <button type="button" class="hs-tab-active:bg-indigo-500/10 dark:hs-tab-active:text-indigo-400 hs-tab-active:text-indigo-500 py-2.5 px-4 inline-flex items-center gap-2.5 text-sm font-medium text-center dark:text-gray-400 text-gray-600 rounded-md dark:hover:text-gray-200 hover:text-indigo-500 focus:outline-none focus:text-gray-700 disabled:opacity-50 disabled:pointer-events-none transition ease-in-out duration-300" id="components-item" data-tab-id="components-tab" data-hs-tab="#components-tab" aria-controls="components-tab" role="tab">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 7.125C2.25 6.504 2.754 6 3.375 6h6c.621 0 1.125.504 1.125 1.125v3.75c0 .621-.504 1.125-1.125 1.125h-6a1.125 1.125 0 0 1-1.125-1.125v-3.75ZM14.25 8.625c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v8.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 0 1-1.125-1.125v-8.25ZM3.75 16.125c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 0 1-1.125-1.125v-2.25Z" />
+      </svg>
+      Components
+    </button>
+    <button type="button" class="hs-tab-active:bg-indigo-500/10 dark:hs-tab-active:text-indigo-400 hs-tab-active:text-indigo-500 py-2.5 px-4 inline-flex items-center gap-2.5 text-sm font-medium text-center dark:text-gray-400 text-gray-600 rounded-md dark:hover:text-gray-200 hover:text-indigo-500 focus:outline-none focus:text-gray-700 disabled:opacity-50 disabled:pointer-events-none transition ease-in-out duration-300" id="forms-item" data-tab-id="forms-tab" data-hs-tab="#forms-tab" aria-controls="forms-tab" role="tab">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 3.75V16.5L12 14.25 7.5 16.5V3.75m9 0H18A2.25 2.25 0 0 1 20.25 6v12A2.25 2.25 0 0 1 18 20.25H6A2.25 2.25 0 0 1 3.75 18V6A2.25 2.25 0 0 1 6 3.75h1.5m9 0h-9" />
+      </svg>
+      Forms
+    </button>
+  </nav>
+</div>
+<!-- End of Navigation Buttons -->
